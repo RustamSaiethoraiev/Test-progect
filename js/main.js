0313a6f668cc4b84d217c371525369a820f21d57
Rustam Saiethoraiev <rustam1822@ukr.net>
2020-11-29 01:51:38 +0200

    sidebar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+// Show more cards
+const btnShowMoreCards = document.querySelector('.btn-more');
+const cards = document.querySelectorAll('.card__link__hidden');
+// console.log(cards);
+btnShowMoreCards.onclick = function () {
+    cards.forEach(card => card.classList.remove('card__link__hidden'));
+}
+//Show or hide the widgts body content
+const widgets = document.querySelectorAll('.widget');
+console.log(widgets);
+widgets.forEach(function (widget) {
+    widget.addEventListener('click', function (e) {
+        if (e.target.classList.contains('widget__title')) {
+            console.log("click");
+            e.target.classList.toggle('widget__title__active');
+            e.target.nextElementSibling.classList.toggle('widget__body__hidden');
+        }
+    });
+})
 
